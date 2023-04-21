b603fe7cc73f8de1911cbec840833cea8bb2e358
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2023-04-21 09:48:54 +0900

    refactor: es6 import apply

--- bin/rock-scissors-paper/rock-scissors-paper.js
+++ bin/rock-scissors-paper/rock-scissors-paper.js
@@ -1,45 +1,45 @@
-#!/usr/bin/env node
+import readline from "readline";
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
+export const RockScissorsPaper = async () => {
+  const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
-});
+    output: process.stdout,
+  });
 
-function randomChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
+  function randomChoice() {
+    const choices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-}
+  }
 
-function compare(userChoice, computerChoice) {
+  function compare(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
-        return "비겼습니다!";
+      return "비겼습니다!";
     } else if (userChoice === "바위") {
-        if (computerChoice === "paper") {
-            return "컴퓨터가 이겼습니다!";
-        } else {
-            return "사용자가 이겼습니다!";
-        }
+      if (computerChoice === "paper") {
+        return "컴퓨터가 이겼습니다!";
+      } else {
+        return "사용자가 이겼습니다!";
+      }
     } else if (userChoice === "보") {
-        if (computerChoice === "scissors") {
-            return "컴퓨터가 이겼습니다!";
-        } else {
-            return "사용자가 이겼습니다!";
-        }
+      if (computerChoice === "scissors") {
+        return "컴퓨터가 이겼습니다!";
+      } else {
+        return "사용자가 이겼습니다!";
+      }
     } else if (userChoice === "가위") {
-        if (computerChoice === "rock") {
-            return "컴퓨터가 이겼습니다!";
-        } else {
-            return "사용자가 이겼습니다!";
-        }
+      if (computerChoice === "rock") {
+        return "컴퓨터가 이겼습니다!";
+      } else {
+        return "사용자가 이겼습니다!";
+      }
     }
-}
+  }
 
-rl.question("가위, 바위, 보! ", (userChoice) => {
+  rl.question("가위, 바위, 보! ", (userChoice) => {
     const computerChoice = randomChoice();
     console.log(`컴퓨터: ${computerChoice}`);
     console.log(compare(userChoice, computerChoice));
     rl.close();
-});
+  });
+};
