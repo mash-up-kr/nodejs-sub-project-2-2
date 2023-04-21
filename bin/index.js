cbbc99f78165043ec96d3f4338872a020d2da521
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2023-04-21 09:49:49 +0900

    feat: game packaging

---
+++ bin/index.js
@@ -0,0 +1,6 @@
+import { Bem } from "../bem-game/bem";
+import { BaseBall } from "../number-baseball/baseball";
+import { RockScissorsPaper } from "../rock-scissors-paper/rock-scissors-paper";
+import { TextAdventures } from "../text-adventures";
+
+export { Bem, BaseBall, RockScissorsPaper, TextAdventures };
