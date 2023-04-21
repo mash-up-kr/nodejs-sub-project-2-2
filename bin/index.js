87079a5c68d133a34cb385bf377e85ca20812bb3
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2023-04-21 23:48:18 +0900

    feature: game input readline setting

--- bin/index.js
+++ bin/index.js
@@ -10,23 +10,26 @@ const Games = async () => {
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question(
-    "게임을 선택하세요. (1. 숫자야구 2. 가위바위보 3. 끝내기 4. BEM 5. 텍스트 어드벤처) ",
-    (answer) => {
-      if (answer === "1") {
-        BaseBall();
-      } else if (answer === "2") {
-        RockScissorsPaper();
-      } else if (answer === "3") {
-        rl.close();
-      } else if (answer === "4") {
-        Bem();
-      } else if (answer === "5") {
-        TextAdventures();
-      }
-      rl.close();
-    }
+  console.log(
+    "게임을 선택하세요. (1. 숫자야구 2. 가위바위보 3. 끝내기 4. BEM 5. 텍스트 어드벤처) "
   );
+  rl.on("line", async (input) => {
+    const answer = input;
+    if (answer === "1") {
+      BaseBall();
+    } else if (answer === "2") {
+      RockScissorsPaper();
+    } else if (answer === "3") {
+      rl.close();
+    } else if (answer === "4") {
+      Bem();
+    } else if (answer === "5") {
+      await TextAdventures();
+    }
+    console.log(
+      "게임을 선택하세요. (1. 숫자야구 2. 가위바위보 3. 끝내기 4. BEM 5. 텍스트 어드벤처) "
+    );
+  });
 };
 
 Games();
